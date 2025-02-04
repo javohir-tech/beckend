7a94c1ac4ae6b199b0d3e84c719eeca58b4a6a7e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-02-05 01:54:56 +0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
+// GET POST DELETE PUT
 const express = require('express')
 
 const app = express()
+
+app.use(express.json())
+
+
+app.get("/", (req, res)=>{
+    res.status(200).send("Hello Javohir")
+})
+
+app.post("/", (req, res)=>{
+    const {name, age} = req.body;
+    const message = `salom  mening ismim ${name}, yoshim ${age} da  `
+    res.send(message)
+})
+
+app.delete("/:id", (req, res)=>{
+    const {id} = req.params
+    res.send(`salom  ${id}`)
+})
+
+app.put("/:id", (req, res)=>{
+   const  {id} = req.params;
+   const  body = req.body
+
+   res.send({id,  body})
+})
+
+
+app.get("/post", (req, res)=>{
+    res.status(200).send("salom post")
+})
 
 const PORT = 5173;
 
