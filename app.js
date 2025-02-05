15edd5d94b8a668680a611ae0f7170a2c169d7f7
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-02-05 22:54:45 +0500

    update

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 // GET POST DELETE PUT
 const express = require('express')
+const mongoose = require("mongoose")
+const postModel = require('./models/post.model')
+
+require("dotenv").config()
 
 const app = express()
 
 app.use(express.json())
 
 
-app.get("/", (req, res)=>{
-    res.status(200).send("Hello Javohir")
+app.get("/", async (req, res)=>{
+    try {
+        const allPosts = await postModel.find()
+        res.status(200).json(allPosts)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+    
 })
 
-app.post("/", (req, res)=>{
-    const {name, age} = req.body;
-    const message = `salom  mening ismim ${name}, yoshim ${age} da  `
-    res.send(message)
+app.post("/", async (req, res)=>{
+    try {
+        const {title, body} = req.body;
+        const newPost = await postModel.create({title, body})
+        res.status(201).json(newPost)   
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 app.delete("/:id", (req, res)=>{
@@ -31,10 +45,22 @@ app.put("/:id", (req, res)=>{
 
 app.get("/post", (req, res)=>{
     res.status(200).send("salom post")
-})
+})     
 
-const PORT = 5173;
+const PORT = process.env.PORT || 5173;
 
-app.listen(PORT, ()=>{
-    console.log(`Listening on http://localhost:${PORT}`)
-})
+
+const startApp = async ()=>{
+    try{
+        await mongoose.connect(process.env.DB_URl).then(()=>{
+            console.log("Contected db")
+        })
+        app.listen(PORT, ()=>{
+            console.log(`Listening on http://localhost:${PORT}`)
+        })
+    }catch(error){
+        console.log(`Error conect with DB : ${error}`)
+    }
+}
+
+startApp()
